172a93e70912f3564a8c4b3609de36e55cd98229
danBamikiya <dudeawesome732@gmail.com>
2021-03-25 12:28:01 +0100

    WIP: add more styling to the chat messages :construction:

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as React from 'react'
 import './App.css'
 import {
   ApolloClient,
@@ -9,7 +9,7 @@ import {
 } from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'http://localhost/4040',
+  uri: 'http://localhost:4040',
   cache: new InMemoryCache()
 })
 
@@ -26,6 +26,11 @@ interface MessagesData {
   messages: Messages[]
 }
 
+interface ChatBubbleProps {
+  sender: string
+  content: string
+}
+
 const GET_MESSAGES = gql`
   query {
     messages {
@@ -37,25 +42,57 @@ const GET_MESSAGES = gql`
 `
 
 function App() {
-  const userMessages = ({ id, user: messageUser, content }: Messages) => {
-    return <>{content}</>
+  const ChatBubble: React.FC<ChatBubbleProps> = ({ sender, content }) => {
+    const mode = sender
+      ? ['bg-blue', 'flex-end']
+      : ['bg-blue-dark', 'flex-start']
+    const classes = mode.join(' ')
+
+    return (
+      <div className={`flex mb-4 ${classes}`}>
+        <div
+          className={
+            'h-11 w-11 mr-3 rounded-3xl text-center text-base border-2 border-solid border-blue'
+          }
+        >
+          {sender.slice(0, 2).toUpperCase()}
+        </div>
+        <div
+          style={{ maxWidth: '60%' }}
+          className={'p-4 text-primary rounded-lg'}
+        >
+          {content}
+        </div>
+      </div>
+    )
   }
+
+  const userMessages = ({ id, user: messageUser, content }: Messages) => (
+    <ChatBubble key={id} content={content} sender={messageUser} />
+  )
 
   const Messages = ({ user }: User) => {
     const { loading, data } = useQuery<MessagesData>(GET_MESSAGES)
+    // if (loading) {
+    //   return <p>Loading ...</p>
+    // } else if (!data) {
+    //   return <p>No message</p>
+    // } else {
+    //   return userMessages(data.messages[0])
+    // }
+
     return (
       <div>
-        <h1>Messages</h1>
         {loading ? (
           <p>Loading ...</p>
         ) : (
-          <h3>
+          <>
             {data?.messages.map(userMessages) || (
               <>
                 <p>No message</p>
               </>
             )}
-          </h3>
+          </>
         )}
       </div>
     )
@@ -63,8 +100,8 @@ function App() {
 
   return (
     <ApolloProvider client={client}>
-      <div className="App">
-        <Messages user="Damian" />
+      <div className='container mx-auto p-6'>
+        <Messages user='Ajoudi' />
       </div>
     </ApolloProvider>
   )
