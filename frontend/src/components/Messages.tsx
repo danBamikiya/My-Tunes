0002270fdc92e3dacb2eaeb1ffa6a18f67835050
danBamikiya <dudeawesome732@gmail.com>
2021-03-26 00:41:29 +0100

    refactor: abstract chat messages ui :recycle:

---
+++ frontend/src/components/Messages.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useQuery, gql } from '@apollo/client'
+import { ChatBubble } from '../ui/ChatBubble'
+
+interface User {
+  user: string
+}
+
+interface MessagesProps extends User {
+  id: number
+  content: string
+}
+
+interface MessagesData {
+  messages: MessagesProps[]
+}
+
+const GET_MESSAGES = gql`
+  query {
+    messages {
+      id
+      content
+      user
+    }
+  }
+`
+
+export const Messages = ({ user }: User) => {
+  const { loading, data } = useQuery<MessagesData>(GET_MESSAGES)
+
+  const userMessages = ({ id, user: messageUser, content }: MessagesProps) => (
+    <ChatBubble key={id} user={user} content={content} sender={messageUser} />
+  )
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading ...</p>
+      ) : (
+        <>
+          {data?.messages.map(userMessages) || (
+            <>
+              <p>No message</p>
+            </>
+          )}
+        </>
+      )}
+    </div>
+  )
+}
