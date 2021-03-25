e41a1b710c39e17560ef0d42bec347b7bc6c4ae6
danBamikiya <dudeawesome732@gmail.com>
2021-03-26 00:28:34 +0100

    refactor: abstract chat bubble ui :recycle:

---
+++ frontend/src/ui/ChatBubble.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+interface ChatBubbleProps {
+  user: string
+  sender: string
+  content: string
+}
+
+export const ChatBubble: React.FC<ChatBubbleProps> = ({
+  user,
+  sender,
+  content
+}) => {
+  const classes =
+    sender === user
+      ? { bg: 'bg-blue', flex: 'justify-end' }
+      : { bg: 'bg-blue-dark', flex: 'justify-start' }
+
+  return (
+    <div className={`flex mb-4 ${classes.flex}`}>
+      {sender !== user && (
+        <div
+          style={{ borderWidth: '1px' }}
+          className={
+            'h-11 w-11 p-1.5 mr-3 rounded-3xl text-center text-base border-solid border-blue-dark'
+          }
+        >
+          {user.slice(0, 2).toUpperCase()}
+        </div>
+      )}
+      <div
+        style={{ maxWidth: '60%' }}
+        className={`p-4 text-primary rounded-lg ${classes.bg}`}
+      >
+        {content}
+      </div>
+    </div>
+  )
+}
