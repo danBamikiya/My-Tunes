2be8e05e345b2e1a43751acb00a9394ad4291974
danBamikiya <dudeawesome732@gmail.com>
2021-03-25 12:44:01 +0100

    feat: add custom styles :lipstick:

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,31 @@ module.exports = {
   purge: ['./src/**/*.{ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {}
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      blue: {
+        light: '#85d7ff',
+        DEFAULT: '#477eff',
+        dark: '#576c99',
+        darker: '#12172d',
+        darkest: '#060415'
+      },
+      pink: {
+        light: '#ff7ce5',
+        DEFAULT: '#ff49db',
+        dark: '#ff16d1'
+      }
+    },
+    textColor: {
+      primary: '#ffffff',
+      secondary: '#ffffff99'
+    },
+    extend: {
+      maxWidth: {
+        'max-w-screen-2xl': '1800px'
+      }
+    }
   },
   variants: {
     extend: {}
