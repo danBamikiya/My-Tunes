fe5bf8aafef464071fd102c3854af8ee81475de2
danBamikiya <dudeawesome732@gmail.com>
2021-03-29 15:08:27 +0100

    style: use ES6 function expressions instead :lipstick:

--- frontend/src/components/ChatInput.tsx
+++ frontend/src/components/ChatInput.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { ChatInputBar } from '../ui/ChatInputBar'
 import { SendBtn } from '../ui/SendButton'
 
-export function ChatInput() {
+export const ChatInput = () => {
   return (
     <div className='flex justify-between items-center chat-input mt-auto rounded-lg p-3.5 bg-blue-darkest'>
       <ChatInputBar />
