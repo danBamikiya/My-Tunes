42f50172906c06a5b3dd033f2e6daa57a8874ab9
danBamikiya <dudeawesome732@gmail.com>
2021-03-30 17:32:54 +0100

    feat: send button can now send message to server when clicked :sparkles:

--- frontend/src/ui/SendButton.tsx
+++ frontend/src/ui/SendButton.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { SendIcon } from '../icons/SendBtnIcon'
 
-export const SendBtn = () => {
+export interface SendBtnProps {
+  sendMsg: () => void
+}
+
+export const SendBtn: React.FC<SendBtnProps> = ({ sendMsg }) => {
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    sendMsg()
+  }
+
   return (
     <button
       style={{ padding: '0 10px' }}
       className='h-8 text-primary bg-blue border-none rounded text-sm leading-4'
+      onClick={handleClick}
     >
       Send
       <SendIcon />
