d74c5196ed789e708d7bf65ecb9e906d906865ad
danBamikiya <dudeawesome732@gmail.com>
2021-03-30 17:48:09 +0100

    refactor: handle passed down events :recycle:

--- frontend/src/lib/handleEvent.ts
+++ frontend/src/lib/handleEvent.ts
@@ -1,14 +1,7 @@
-import React from 'react'
-
-interface handleKeyDownProps {
-  (
-    event: React.KeyboardEvent<HTMLInputElement>,
-    payload: (message: string) => void
-  ): void
+interface handleEventProps {
+  (value: string, payload: (message: string) => void): void
 }
 
-export const handleKeyDown: handleKeyDownProps = (event, payload) => {
-  event.preventDefault()
-  payload(event.currentTarget.value)
-  event.currentTarget.value = '' // Clear Fields
+export const handleEvent: handleEventProps = (value, payload) => {
+  payload(value)
 }
