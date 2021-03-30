7c5097ad338d8b1e83f2a2f14731958f67d60e32
danBamikiya <dudeawesome732@gmail.com>
2021-03-31 00:18:39 +0100

    feat: use graphql subscription :sparkles:

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,17 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 
 interface Message {
   id: number
   user: string
   content: string
+}
+
+interface SubscriptionPubSub {
+  pubsub: PubSub
+}
+
+interface onMessagesUpdatesArgs {
+  (): boolean
 }
 
 const messages: Message[] = []
@@ -22,7 +30,14 @@ const typeDefs = `
   type  Mutation {
       postMessage(user: String!, content: String!): ID!
   }
+
+  type Subscription {
+      messages:  [Message!]
+  }
 `
+
+const subscribers = []
+const onMessagesUpdates = (fn: onMessagesUpdatesArgs) => subscribers.push(fn)
 
 const resolvers = {
   Query: {
@@ -34,10 +49,22 @@ const resolvers = {
       messages.push({ id, user, content })
       return id
     }
+  },
+  Subscription: {
+    messages: {
+      subscribe: (parent: any, args: any, { pubsub }: SubscriptionPubSub) => {
+        const channel = Math.random().toString(36).slice(2, 15) // random channel name
+        onMessagesUpdates(() => pubsub.publish(channel, { messages }))
+        setTimeout(() => pubsub.publish(channel, { messages }), 0)
+        return pubsub.asyncIterator(channel)
+      }
+    }
   }
 }
 const options = { port: 4040 }
-const server = new GraphQLServer({ typeDefs, resolvers })
+const pubsub = new PubSub()
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } })
+
 server.start(options, ({ port }) => {
   console.log(`Server started on http://localhost:${port}`)
 })
