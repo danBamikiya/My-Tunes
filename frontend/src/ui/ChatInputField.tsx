1067dff2514b0878bcee27b2b016b4cc9f1a1806
danBamikiya <dudeawesome732@gmail.com>
2021-03-30 12:24:27 +0100

    fix: change onChange event to onKeyDown event and add its handler :ambulance:

--- frontend/src/ui/ChatInputField.tsx
+++ frontend/src/ui/ChatInputField.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { handleKeyDown } from '../lib/handleKeyDown'
 
-export const ChatInputField = sendMsg => {
+export interface ChatInputFieldProps {
+  sendMsg: (message: string) => void
+}
+
+export const ChatInputField: React.FC<ChatInputFieldProps> = ({ sendMsg }) => {
+  const handleKeyDownEvent = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    handleKeyDown(event, sendMsg)
+  }
+
   return (
     <input
       type='text'
       className='border-none outline-none h-8 flex-1 mr-1 bg-blue-darkest text-primary'
       placeholder='Enter your message here'
-      onChange={sendMsg}
+      onKeyDown={handleKeyDownEvent}
     />
   )
 }
