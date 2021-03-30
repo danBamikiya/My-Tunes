9272520807f53c4b7faf54daa6a9603274780f1b
danBamikiya <dudeawesome732@gmail.com>
2021-03-30 12:31:12 +0100

    feat: add state :sparkles:

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { ChatInput } from '../components/ChatInput'
 import { Messages } from '../components/Messages'
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
@@ -9,11 +9,28 @@ const client = new ApolloClient({
 })
 
 export default function Chat() {
+  const [state, setState] = useState({
+    user: '',
+    content: ''
+  })
+
+  useEffect(() => {
+    console.log(state)
+  }, [state])
+
+  const setCurrentUser = (user: string) => {
+    setState({ ...state, user: user })
+  }
+
+  const sendMessage = (message: string) => {
+    setState({ ...state, content: message })
+  }
+
   return (
     <ApolloProvider client={client}>
       <div className='container mx-auto p-6'>
         <Messages user='Damian' />
-        <ChatInput />
+        <ChatInput setCurrentUser={setCurrentUser} sendMsg={sendMessage} />
       </div>
     </ApolloProvider>
   )
