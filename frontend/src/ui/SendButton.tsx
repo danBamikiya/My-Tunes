49c034ddae53d886bf725288c14e4929139cf987
danBamikiya <dudeawesome732@gmail.com>
2021-03-30 13:56:06 +0100

    fix: change button spacing :lipstick:

--- frontend/src/ui/SendButton.tsx
+++ frontend/src/ui/SendButton.tsx
@@ -4,11 +4,8 @@ import { SendIcon } from '../icons/SendBtnIcon'
 export const SendBtn = () => {
   return (
     <button
-      style={{
-        padding: '0 32px 0 10px',
-        backgroundPosition: 'center right 8px'
-      }}
-      className='h-8 text-primary bg-blue border-none rounded text-xs leading-4'
+      style={{ padding: '0 10px' }}
+      className='h-8 text-primary bg-blue border-none rounded text-sm leading-4'
     >
       Send
       <SendIcon />
