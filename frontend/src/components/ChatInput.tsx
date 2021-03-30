388c7497cdc4e7f79542e99c387242f90736b133
danBamikiya <dudeawesome732@gmail.com>
2021-03-30 12:28:55 +0100

    fix: add onKeyDown event handler :ambulance:

--- frontend/src/components/ChatInput.tsx
+++ frontend/src/components/ChatInput.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
 import { ChatInputBar } from '../ui/ChatInputBar'
+import { ChatInputFieldProps } from '../ui/ChatInputField'
 import { SendBtn } from '../ui/SendButton'
+import { handleKeyDown } from '../lib/handleKeyDown'
 
-export const ChatInput = () => {
+interface ChatInputProps extends ChatInputFieldProps {
+  setCurrentUser: (user: string) => void
+}
+
+export const ChatInput: React.FC<ChatInputProps> = ({
+  setCurrentUser,
+  sendMsg
+}) => {
+  const handleKeyDownEvent = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    handleKeyDown(event, setCurrentUser)
+  }
+
   return (
     <div className='flex justify-between items-center chat-input mt-auto rounded-lg p-3.5 bg-blue-darkest'>
-      <ChatInputBar />
+      <input
+        type='text'
+        // value={state.user}
+        placeholder='Change User'
+        style={{ padding: '0 6px 0 12px' }}
+        className='h-8 border-none bg-blue-darker text-primary rounded'
+        onKeyDown={handleKeyDownEvent}
+      />
+      <ChatInputBar sendMsg={sendMsg} />
       <SendBtn />
     </div>
   )
