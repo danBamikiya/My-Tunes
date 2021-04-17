35b2b3c5df1542c12551064dc7a4e93d8ce36a52
danBamikiya <dudeawesome732@gmail.com>
2021-04-18 00:00:01 +0100

    add: brand logo :lipstick:

---
+++ frontend/src/ui/BrandLogo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const BrandLogo = () => (
+  <a
+    href='/'
+    style={{ textDecoration: 'none' }}
+    className={'flex items-center text-xl text-primary text'}
+  >
+    <img alt='soundchat logo' />
+    <div className={'ml-4'}>Soundchat</div>
+  </a>
+)
