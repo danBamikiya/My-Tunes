8a883f3aca2929a4feec3813bf7f05794b5fed86
danBamikiya <dudeawesome732@gmail.com>
2021-04-18 00:20:01 +0100

    add: home nav link :lipstick:

---
+++ frontend/src/ui/HomeNav.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const HomeNav = () => (
+  <nav>
+    <ul className='flex list-none'>
+      <li>
+        <a
+          style={{ textDecoration: 'none' }}
+          href='/login'
+          className='text-primary p-4 home-nav-link'
+        ></a>
+      </li>
+    </ul>
+  </nav>
+)
