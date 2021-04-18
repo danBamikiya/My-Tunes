014c488d4fa2bc18b9323c9357c8a64407c097d5
danBamikiya <dudeawesome732@gmail.com>
2021-04-18 16:38:42 +0100

    feat: use children in Layout component :sparkles:

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
 interface LayoutProps {
-  children: React.ReactChild
-  // React.ReactElement
-  mainClass: string[]
+  children: React.ReactNode
+  pageClass: string
+  pageStyle?: {
+    backgroundColor: string
+  }
 }
 
-export const Layout: React.FC<LayoutProps> = ({ children, mainClass }) => (
-  <main role='main' className={`container mx-auto ${mainClass}`}>
+export const Layout: React.FC<LayoutProps> = ({
+  children,
+  pageClass,
+  pageStyle
+}) => (
+  <main
+    role='main'
+    style={pageStyle}
+    className={`container mx-auto ${pageClass}`}
+  >
     {children}
   </main>
 )
