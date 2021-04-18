cdcc6911065aa1b955d45e359e7d02bfd31ff9af
danBamikiya <dudeawesome732@gmail.com>
2021-04-18 19:01:50 +0100

    add: login page :sparkles:

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,21 @@
 import React from 'react'
 
+import { Layout } from '../layout/Layout'
+import { Header } from '../components/Header'
+
+import '../styles/Login.css'
+
 export default function Login() {
-  return <div>Login</div>
+  const login = {
+    loginClass: 'login-global px-10 flex-grow flex flex-col',
+    loginStyle: {
+      backgroundColor: '#2f303a'
+    }
+  }
+
+  return (
+    <Layout pageClass={login.loginClass} pageStyle={login.loginStyle}>
+      <Header />
+    </Layout>
+  )
 }
