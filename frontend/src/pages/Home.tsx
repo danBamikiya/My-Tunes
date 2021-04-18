4e43b743114a797a9ccfde1aac2e6be4067d3343
danBamikiya <dudeawesome732@gmail.com>
2021-04-18 08:38:35 +0100

    add: components to home page :lipstick:

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+import { HomeMainSection } from '../components/HomeMainSection'
+import { MainHeader } from '../components/MainHeader'
+
 export default function Home() {
-  return <div>Hello</div>
+  return (
+    <div className='container mx-auto px-10 flex-grow flex flex-col'>
+      <MainHeader />
+      <HomeMainSection />
+    </div>
+  )
 }
