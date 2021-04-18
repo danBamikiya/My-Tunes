3bc65863ae06d407833bb67422008ee4b1871abc
danBamikiya <dudeawesome732@gmail.com>
2021-04-18 17:13:15 +0100

    refactor: remove unnecessary style props :recycle:

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -3,21 +3,10 @@ import React from 'react'
 interface LayoutProps {
   children: React.ReactNode
   pageClass: string
-  pageStyle?: {
-    backgroundColor: string
-  }
 }
 
-export const Layout: React.FC<LayoutProps> = ({
-  children,
-  pageClass,
-  pageStyle
-}) => (
-  <main
-    role='main'
-    style={pageStyle}
-    className={`container mx-auto ${pageClass}`}
-  >
+export const Layout: React.FC<LayoutProps> = ({ children, pageClass }) => (
+  <main role='main' className={`container mx-auto ${pageClass}`}>
     {children}
   </main>
 )
