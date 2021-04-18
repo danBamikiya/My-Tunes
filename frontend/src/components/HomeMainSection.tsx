820aeacc15210ccaf4db0c91c7f608b452bf3be9
danBamikiya <dudeawesome732@gmail.com>
2021-04-18 08:37:26 +0100

    WIP: add more elements to home main section :construction:

--- frontend/src/components/HomeMainSection.tsx
+++ frontend/src/components/HomeMainSection.tsx
@@ -1,17 +1,23 @@
 import React from 'react'
 
 export const HomeMainSection = () => (
-  <section>
+  <section className='flex items-stretch justify-between flex-grow'>
     <div className='flex-col flex flex-grow items-stretch justify-end'>
       <div className='lady-image'></div>
-      {/* we are using a div for the image instead of the img tag so that we can trickishly scale and position the image */}
+      {/* a div is being used for the image instead of the img tag so that it can be easily scaled and positioned */}
     </div>
-    <div className='call-to-action'>
-      <h1 className='title'>Feel The Music</h1>
-      <span className='subtitle'>
-        Stream over 20 thousand songs with one click
+    <div className='flex flex-col items-start m-4 self-center'>
+      <h1 style={{ fontSize: '3.5rem' }} className='font-medium mb-4 mt-0'>
+        Feel The Music
+      </h1>
+      <span style={{ fontSize: '1.4rem' }} className='font-medium'>
+        Share songs with friends with one click
       </span>
-      <a href='/login' className='btn'>
+      <a
+        style={{ textDecoration: 'none' }}
+        href='/login'
+        className='text-primary bg-blue-btn py-3 px-6 rounded-lg text-sm outline-none border-none cursor-pointer btn'
+      >
         Join Now
       </a>
     </div>
