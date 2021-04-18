68e1ea817019a623b49318e63064548793795711
danBamikiya <dudeawesome732@gmail.com>
2021-04-18 17:38:45 +0100

    feat: use home page custom styling :lipstick:

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,6 +4,8 @@ import { Layout } from '../layout/Layout'
 import { HomeMainSection } from '../components/HomeMainSection'
 import { MainHeader } from '../components/MainHeader'
 
+import '../styles/Home.css'
+
 export default function Home() {
   const home = {
     homeClass: 'home-global px-10 flex-grow flex flex-col'
