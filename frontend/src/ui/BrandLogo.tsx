1366ec685837e2e917676431032ed332c7f10aa5
danBamikiya <dudeawesome732@gmail.com>
2021-04-18 15:46:10 +0100

    refactor: cleanup dead classname :wastebasket:

--- frontend/src/ui/BrandLogo.tsx
+++ frontend/src/ui/BrandLogo.tsx
@@ -4,7 +4,7 @@ export const BrandLogo = () => (
   <a
     href='/'
     style={{ textDecoration: 'none' }}
-    className={'flex items-center text-xl text-primary text'}
+    className={'flex items-center text-xl text-primary'}
   >
     <img alt='soundchat logo' />
     <div className={'ml-4'}>Soundchat</div>
