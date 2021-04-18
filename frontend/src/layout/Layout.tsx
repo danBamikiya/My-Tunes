bc6a482b5e0216da62490b51d57f8a088d803b16
danBamikiya <dudeawesome732@gmail.com>
2021-04-18 17:57:21 +0100

    feat: use style props for global wrapper :lipstick:

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -3,10 +3,19 @@ import React from 'react'
 interface LayoutProps {
   children: React.ReactNode
   pageClass: string
+  pageStyle?: {
+    backgroundColor: string
+  }
 }
 
-export const Layout: React.FC<LayoutProps> = ({ children, pageClass }) => (
-  <main role='main' className={`container mx-auto ${pageClass}`}>
-    {children}
-  </main>
+export const Layout: React.FC<LayoutProps> = ({
+  children,
+  pageClass,
+  pageStyle
+}) => (
+  <div style={pageStyle} className='h-screen'>
+    <main role='main' className={`container mx-auto ${pageClass} h-full`}>
+      {children}
+    </main>
+  </div>
 )
