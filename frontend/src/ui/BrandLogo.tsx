18e7c7af0d0839a2f07d90bdf9b02c57f5b05218
danBamikiya <dudeawesome732@gmail.com>
2021-04-21 23:25:53 +0100

    fix: use class instead of style attribute :adhesive_bandage:

--- frontend/src/ui/BrandLogo.tsx
+++ frontend/src/ui/BrandLogo.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
 
 export const BrandLogo = () => (
-  <a
-    href='/'
-    style={{ textDecoration: 'none' }}
-    className={'flex items-center text-xl'}
-  >
+  <a href='/' className={'flex items-center text-xl no-underline'}>
     <img alt='soundchat logo' />
     <div className={'ml-4'}>Soundchat</div>
   </a>
