c4be4ec8eb5078a3ce2485239d9626249580a352
danBamikiya <dudeawesome732@gmail.com>
2021-04-26 15:20:34 +0100

    feat: add login main section to login page :lipstick:

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { Layout } from '../layout/Layout'
 import { Header } from '../components/Header'
+import { LoginMainSection } from '../components/LoginMainSection'
 
 import '../styles/Login.css'
 
@@ -16,6 +17,7 @@ export default function Login() {
   return (
     <Layout pageClass={login.loginClass} pageStyle={login.loginStyle}>
       <Header />
+      <LoginMainSection />
 
       <div className='login-page-circle-1'></div>
       <div className='login-page-circle-2'></div>
