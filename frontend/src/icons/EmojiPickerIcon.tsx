97b0f1549be2e5fe68b43afb92c6f30df34b3e28
danBamikiya <dudeawesome732@gmail.com>
2021-04-26 15:32:12 +0100

    style: improve structure EmojiPickerIcon ui file :art:

--- frontend/src/icons/EmojiPickerIcon.tsx
+++ frontend/src/icons/EmojiPickerIcon.tsx
@@ -1,21 +1,19 @@
 import React from 'react'
 
-export const EmojiPickerIcon = () => {
-  return (
-    <svg
-      xmlns='http://www.w3.org/2000/svg'
-      width='20'
-      height='20'
-      fill='none'
-      stroke='currentColor'
-      strokeLinecap='round'
-      strokeLinejoin='round'
-      strokeWidth='2'
-      viewBox='0 0 24 24'
-    >
-      <defs></defs>
-      <circle cx='12' cy='12' r='10'></circle>
-      <path d='M8 14s1.5 2 4 2 4-2 4-2M9 9h.01M15 9h.01'></path>
-    </svg>
-  )
-}
+export const EmojiPickerIcon = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='20'
+    height='20'
+    fill='none'
+    stroke='currentColor'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    strokeWidth='2'
+    viewBox='0 0 24 24'
+  >
+    <defs></defs>
+    <circle cx='12' cy='12' r='10'></circle>
+    <path d='M8 14s1.5 2 4 2 4-2 4-2M9 9h.01M15 9h.01'></path>
+  </svg>
+)
