f689a977494b65ef86d6d5e02075049f76e01c84
danBamikiya <dudeawesome732@gmail.com>
2021-04-28 03:00:44 +0100

    refactor: remove unnecessary props from Home page :recycle:

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -7,15 +7,8 @@ import { Header } from '../components/Header'
 import '../styles/Home.css'
 
 export default function Home() {
-  const home = {
-    homeClass: 'home-global px-10 flex-grow flex flex-col',
-    homeStyle: {
-      backgroundColor: '#2f303a'
-    }
-  }
-
   return (
-    <Layout pageClass={home.homeClass} pageStyle={home.homeStyle}>
+    <Layout>
       <Header />
       <HomeMainSection />
 
