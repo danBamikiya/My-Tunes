a9c277c6065cfa5dc887ee4b18f514de3a22d9cf
danBamikiya <dudeawesome732@gmail.com>
2021-04-28 03:40:23 +0100

    add: app logo to header :bento:

---
+++ frontend/src/icons/SoundChatIcon.tsx
@@ -0,0 +1,332 @@
+import React from 'react'
+
+type SoundChatIconProps = React.SVGProps<SVGSVGElement> & {
+  classnames?: string
+}
+
+export const SoundChatIcon: React.FC<SoundChatIconProps> = ({ classnames }) => (
+  <svg
+    width='81'
+    height='81'
+    viewBox='0 0 81 81'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    className={classnames}
+  >
+    <path
+      d='M55.8473 62.651L68.3471 74.2694C68.6669 74.5667 69.1875 74.3398 69.1875 73.9032L69.1875 50.6663C69.1875 50.2296 68.6669 50.0028 68.3471 50.3001L55.8473 61.9185C55.6345 62.1163 55.6345 62.4532 55.8473 62.651Z'
+      stroke='url(#paint0_linear)'
+      strokeWidth='3'
+    />
+    <rect
+      x='-1.5'
+      y='1.5'
+      width='42'
+      height='37.4861'
+      rx='8.5'
+      transform='matrix(-1 0 0 1 78 26)'
+      stroke='url(#paint1_linear)'
+      strokeWidth='3'
+    />
+    <path
+      d='M67.7004 62.0001H61.7004V65.0001C61.7004 66.1046 62.5958 67.0001 63.7004 67.0001H67.7004V62.0001Z'
+      fill='#2F303A'
+    />
+    <path
+      d='M53.0004 62.5C53.0004 63.0523 53.4481 63.5 54.0004 63.5H72.0004C73.1049 63.5 74.0004 62.6046 74.0004 61.5V46.5H53.0004V62.5Z'
+      fill='#2F303A'
+    />
+    <rect
+      width='3.40673'
+      height='5.44289'
+      transform='matrix(-0.688061 0.725654 0.712956 0.701209 61.4894 60.9789)'
+      fill='#2F303A'
+    />
+    <mask
+      id='mask0'
+      mask-type='alpha'
+      maskUnits='userSpaceOnUse'
+      x='42'
+      y='39'
+      width='31'
+      height='16'
+    >
+      <line
+        x1='1.5'
+        y1='-1.5'
+        x2='4.61111'
+        y2='-1.5'
+        transform='matrix(0 1 1 0 72.75 44.3334)'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='1.5'
+        y1='-1.5'
+        x2='4.61111'
+        y2='-1.5'
+        transform='matrix(0 1 1 0 45.75 44.3334)'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='1.5'
+        y1='-1.5'
+        x2='4.61111'
+        y2='-1.5'
+        transform='matrix(0 1 1 0 69.375 44.3334)'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='1.5'
+        y1='-1.5'
+        x2='4.61111'
+        y2='-1.5'
+        transform='matrix(0 1 1 0 49.125 44.3334)'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='1.5'
+        y1='-1.5'
+        x2='9.19444'
+        y2='-1.5'
+        transform='matrix(0 1 1 0 66 42.0417)'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='1.5'
+        y1='-1.5'
+        x2='9.19444'
+        y2='-1.5'
+        transform='matrix(0 1 1 0 53.175 42.0417)'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='1.5'
+        y1='-1.5'
+        x2='13.0139'
+        y2='-1.5'
+        transform='matrix(0 1 1 0 61.95 39.75)'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='1.5'
+        y1='-1.5'
+        x2='13.0139'
+        y2='-1.5'
+        transform='matrix(0 1 1 0 57.225 39.75)'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+    </mask>
+    <g mask='url(#mask0)'>
+      <rect
+        width='33.75'
+        height='16.0417'
+        transform='matrix(-1 0 0 1 73.5 39.75)'
+        fill='url(#paint2_linear)'
+      />
+    </g>
+    <path d='M26 19H63V46C63 51.5228 58.5228 56 53 56H26V19Z' fill='#2F303A' />
+    <path
+      d='M34.3701 47.8693L16.087 64.5541C15.7661 64.8471 15.25 64.6194 15.25 64.1848L15.25 30.8152C15.25 30.3807 15.7661 30.1529 16.087 30.4459L34.3701 47.1307C34.5873 47.329 34.5873 47.671 34.3701 47.8693Z'
+      stroke='url(#paint3_linear)'
+      strokeWidth='3'
+    />
+    <rect
+      x='1.5'
+      y='1.5'
+      width='57'
+      height='50'
+      rx='8.5'
+      stroke='url(#paint4_linear)'
+      strokeWidth='3'
+    />
+    <rect x='17' y='47' width='8' height='6' fill='#2F303A' />
+    <rect
+      x='16.73'
+      y='54'
+      width='5'
+      height='2'
+      transform='rotate(-90 16.73 54)'
+      fill='#2F303A'
+    />
+    <circle cx='14.5' cy='50.5' r='1.5' fill='#BC3A80' />
+    <rect x='13' y='49' width='3' height='1' fill='#2F303A' />
+    <rect x='9' y='25' width='28' height='25' fill='#2F303A' />
+    <rect
+      x='27.1233'
+      y='46.0242'
+      width='4.7504'
+      height='7.19606'
+      transform='rotate(47.5 27.1233 46.0242)'
+      fill='#2F303A'
+    />
+    <mask
+      id='mask1'
+      mask-type='alpha'
+      maskUnits='userSpaceOnUse'
+      x='11'
+      y='18'
+      width='39'
+      height='19'
+    >
+      <line
+        x1='12.5'
+        y1='25.5'
+        x2='12.5'
+        y2='30.5'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='48.5'
+        y1='25.5'
+        x2='48.5'
+        y2='30.5'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='17'
+        y1='25.5'
+        x2='17'
+        y2='30.5'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='44'
+        y1='25.5'
+        x2='44'
+        y2='30.5'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='21.5'
+        y1='22.5'
+        x2='21.5'
+        y2='33.5'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='38.6'
+        y1='22.5'
+        x2='38.6'
+        y2='33.5'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='26.9'
+        y1='19.5'
+        x2='26.9'
+        y2='35.5'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+      <line
+        x1='33.2'
+        y1='19.5'
+        x2='33.2'
+        y2='35.5'
+        stroke='black'
+        strokeWidth='3'
+        strokeLinecap='round'
+      />
+    </mask>
+    <g mask='url(#mask1)'>
+      <rect x='10' y='18' width='45' height='21' fill='url(#paint5_linear)' />
+    </g>
+    <defs>
+      <linearGradient
+        id='paint0_linear'
+        x1='53.25'
+        y1='62.2847'
+        x2='76.5'
+        y2='62.2847'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#BC3A80' />
+        <stop offset='0.619792' stopColor='#BC3A80' />
+      </linearGradient>
+      <linearGradient
+        id='paint1_linear'
+        x1='22.5'
+        y1='0'
+        x2='22.5'
+        y2='40.4861'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#1762A7' />
+        <stop offset='0.6875' stopColor='#BC3A80' />
+      </linearGradient>
+      <linearGradient
+        id='paint2_linear'
+        x1='16.875'
+        y1='0'
+        x2='16.875'
+        y2='16.0417'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#1762A7' />
+        <stop offset='1' stopColor='#BC3A80' />
+      </linearGradient>
+      <linearGradient
+        id='paint3_linear'
+        x1='37'
+        y1='47.5'
+        x2='6'
+        y2='47.5'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#BC3A80' />
+        <stop offset='0.619792' stopColor='#BC3A80' />
+      </linearGradient>
+      <linearGradient
+        id='paint4_linear'
+        x1='30'
+        y1='0'
+        x2='30'
+        y2='53'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#1762A7' />
+        <stop offset='0.6875' stopColor='#BC3A80' />
+      </linearGradient>
+      <linearGradient
+        id='paint5_linear'
+        x1='32.5'
+        y1='18'
+        x2='32.5'
+        y2='39'
+        gradientUnits='userSpaceOnUse'
+      >
+        <stop stopColor='#1762A7' />
+        <stop offset='1' stopColor='#BC3A80' />
+      </linearGradient>
+    </defs>
+  </svg>
+)
