ccde3c6453dae6578979eab70301b8ff59633342
danBamikiya <dudeawesome732@gmail.com>
2021-04-28 03:02:52 +0100

    fix: add previous global styles to layout wrapper :recycle:

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -5,19 +5,17 @@ import { isPage } from '../lib/isPage'
 
 type LayoutProps = {
   children: React.ReactNode
-  pageClass?: string
-  pageStyle?: {
-    backgroundColor: string
-  }
 }
 
-export const Layout: React.FC<LayoutProps> = ({
-  children,
-  pageClass,
-  pageStyle
-}) => (
-  <div style={pageStyle} className='min-h-screen flex flex-col relative'>
-    <main role='main' className={`container mx-auto ${pageClass} h-full`}>
+export const Layout: React.FC<LayoutProps> = ({ children }) => (
+  <div
+    style={{ backgroundColor: '#2f303a' }}
+    className='min-h-screen flex flex-col relative'
+  >
+    <main
+      role='main'
+      className='container mx-auto px-10 flex-grow flex flex-col h-full'
+    >
       {children}
     </main>
     {isPage('login') ? <LoginFooter /> : <></>}
