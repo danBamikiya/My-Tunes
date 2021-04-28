c798a33e643633c44cb69fe7605b5303d62ad43e
danBamikiya <dudeawesome732@gmail.com>
2021-04-28 02:29:27 +0100

    fix: increase Header component stack order :children_crossing:

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,7 +6,7 @@ import { HeaderNav } from '../ui/HeaderNav'
 export const Header = () => (
   <header
     style={{ height: '140px' }}
-    className='flex justify-between items-center'
+    className='flex justify-between items-center z-10'
   >
     <BrandLogo />
     <HeaderNav />
